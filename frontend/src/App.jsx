103a12bc2cc845c09249f11b99b5a70cf4fda1fd
Cristian <cristian.christensen@gmail.com>
2024-05-07 11:29:50 +0200

    Added SignInForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import SignUpForm from "./components/SignUpForm"
+import SignInForm from "./components/SignInForm"
 
 function App() {
  
   return (
     <>
       <SignUpForm />
+      <SignInForm />
     </>
   )
 }
